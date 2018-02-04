26922d098d921999c314e93e0c81e1e4aa572590
Fanghao(Gabriel) Song <fanghaogab@gmail.com>
2018-02-04 16:21:18 -0500

    HeadFirstChap6Exercise

---
+++ ArrayExercise.java
@@ -0,0 +1,13 @@
+package chap06;
+import java.util.*;
+public class ArrayExercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> f = new ArrayList<String>();
+		f.add(new String("abc"));
+		f.add(new String("abc"));
+		System.out.print(f);
+	}
+
+}
